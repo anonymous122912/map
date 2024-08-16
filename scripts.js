c01130652879e0cdd8d9c28c18fa2a1953a5d874
anonymous <127376439+anonymous122912@users.noreply.github.com>
2024-08-16 20:05:45 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,12 +11,16 @@ const customIcon = L.icon({
 });
 
 let sourceMarker, destinationMarker, movingMarker, routingControl;
+let userRoute = []; // Array to store the user's route
 
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
       const userLocation = [latitude, longitude];
+
+      // Store the user's location in the userRoute array
+      userRoute.push(userLocation);
 
       if (!sourceMarker) {
         sourceMarker = L.marker(userLocation).addTo(map);
@@ -25,6 +29,15 @@ if (navigator.geolocation) {
         movingMarker = L.marker(userLocation, { icon: customIcon }).addTo(map);
       } else {
         movingMarker.setLatLng(userLocation);
+      }
+
+      // Draw the route on the map using a polyline
+      if (userRoute.length > 1) {
+        L.polyline(userRoute, {
+          color: "red",    // Color of the route line
+          weight: 4,       // Thickness of the route line
+          opacity: 0.7,    // Opacity of the route line
+        }).addTo(map);
       }
     },
     (error) => {
@@ -56,3 +69,4 @@ map.on("click", function (e) {
     });
   }
 });
+
